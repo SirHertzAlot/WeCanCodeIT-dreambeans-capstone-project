cc51f90e4b024dbd47849aa0acd08aa50837b17e
RussellWCCIGitHub <134211105+RussellWCCIGitHub@users.noreply.github.com>
2023-09-10 16:12:52 -0400

    created product class

---
+++ src/main/java/com/dream_beans/coffee/ProductClassModel.java
@@ -0,0 +1,55 @@
+package com.dream_beans.coffee;
+
+public class ProductClassModel {
+
+    private int price;
+    private int id;
+    private String description;
+    private String name;
+    private String size;
+
+    public ProductClassModel() {
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+}
