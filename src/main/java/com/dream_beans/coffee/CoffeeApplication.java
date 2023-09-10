14dd6f2fb4e7b73811fe7a49ef4b77465b803e1d
krischielle <krischielle.espiritu@gmail.com>
2023-09-10 15:28:26 -0400

    initial changes

---
+++ src/main/java/com/dream_beans/coffee/CoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.dream_beans.coffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeApplication.class, args);
+	}
+
+}
