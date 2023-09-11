eb262af47364ddc66883f9e475b41e2c86bf6cc9
Jperson259 <jperson22@charter.net>
2023-09-11 12:41:58 -0400

    added manytoone into menu model

--- src/main/java/com/dream_beans/coffee/models/Menu.java
+++ src/main/java/com/dream_beans/coffee/models/Menu.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -28,7 +28,8 @@ public class Menu {
     private String image;
 
 
-    
+    @ManyToOne
+    private Inventory inventory;
 
     public Menu() {
     }
