8a08004716a6f0c7fbdeb587e36363d64edf53ba
Jperson259 <jperson22@charter.net>
2023-09-11 12:34:52 -0400

    added contructors, getters, and setters

---
+++ src/main/java/com/dream_beans/coffee/models/Menu.java
@@ -0,0 +1,89 @@
+package com.dream_beans.coffee.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "menu")
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "price")
+    private float price;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "image")
+    private String image;
+
+
+    
+
+    public Menu() {
+    }
+
+
+
+    public Menu(long id, String name, float price, String description, String image) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+    }
+
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    
+    
+}
