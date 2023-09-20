3c9d77f2e8794d76f3664c35280134b61110d153
RussellWCCIGitHub <134211105+RussellWCCIGitHub@users.noreply.github.com>
2023-09-19 22:15:50 -0400

    Created the Cart Controller-V1

---
+++ src/main/java/com/dream_beans/coffee/CartController.java
@@ -0,0 +1,44 @@
+package com.dream_beans.coffee;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    @Autowired
+    CartRepository repository;
+
+    @GetMapping("/allcarts")
+    public List<Cart> getAllCarts() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cart> getCartById(@PathVariable Long id) {
+        Optional<Cart> cart = repository.findById(id);
+        if (cart.isPresent()) {
+            return ResponseEntity.ok(cart.get());
+        } else {
+            return ResponseEntity.notFound();
+        }
+    }
+
+    // @PostMapping("/addItem")
+
+    // @DeleteMapping("/removeItem/{id}")
+
+    // @PutMapping("/updateQuantity/{id}")
+
+    // @PostMapping("/save")
+
+    // @GetMapping("/size")
+
+}
