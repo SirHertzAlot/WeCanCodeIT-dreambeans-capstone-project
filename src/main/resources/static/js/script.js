a3c6ace468059e53dc089927456ee4679062d29d
krischielle <krischielle.espiritu@gmail.com>
2023-09-21 12:52:42 -0400

    added script to update qty in cart when add to cart is clicked

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,11 +1,26 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", ()=>{
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-document.querySelectorAll(".link").forEach(n => n.addEventListener("click", ()=>{
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+document.querySelectorAll(".link").forEach((n) =>
+  n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
-}))
+  })
+);
+
+
+document.querySelectorAll('.addCart').forEach(button =>{
+    button.addEventListener('click', ()=>{
+        addToCart();
+    })
+})
+function addToCart(){
+    let cartQtyElement = document.querySelector(".cartQuantity");
+    let cartQty = parseInt(cartQtyElement.textContent);
+    cartQty++;
+    cartQtyElement.textContent = cartQty;
+}
