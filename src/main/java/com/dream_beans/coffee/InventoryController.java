4bb76715840c7e1143a3209cd4af8c616a7b85e3
Jperson259 <jperson22@charter.net>
2023-09-21 23:15:28 -0400

    added postmapping. still need to fix

--- src/main/java/com/dream_beans/coffee/InventoryController.java
+++ src/main/java/com/dream_beans/coffee/InventoryController.java
@@ -7,21 +7,30 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class InventoryController {
-    
+
     private InventoryServiceImpl inventoryService;
 
     @RequestMapping(value = "/api/inventory", method = RequestMethod.GET)
-    public ModelAndView listEntireInventory(){
+    public ModelAndView listEntireInventory() {
         ModelAndView coffeeInventory = new ModelAndView("inventory");
         coffeeInventory.addObject("inventory", inventoryService.listEntireInventory());
         return coffeeInventory;
     }
 
     @GetMapping(value = "inventory")
-    ResponseEntity<String> test(){
+    ResponseEntity<String> test() {
         return new ResponseEntity<>("Was this test successful?", HttpStatus.OK);
     }
 
     @ModelAttribute("/api/inventory")
-    public Iterable<Object> inventory() { return inventoryService.listEntireInventory(); }
+    public Iterable<Object> inventory() {
+        return inventoryService.listEntireInventory();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        Product _product = productRepository
+                .save(new Product());
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
