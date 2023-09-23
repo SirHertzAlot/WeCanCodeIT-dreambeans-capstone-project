ebd81f039585dcf2ef3c025862c097d30ba74fb4
jdfuents <jfuentesdiaz@outlook.com>
2023-09-23 16:32:45 -0400

    made new repository

--- src/main/java/com/dream_beans/coffee/Repository/CartRepository.java
+++ src/main/java/com/dream_beans/coffee/Repository/CartRepository.java
@@ -1,6 +1,8 @@
-package com.dream_beans.coffee;
+package com.dream_beans.coffee.Repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+import com.dream_beans.coffee.models.Cart;
 
 public interface CartRepository extends CrudRepository<Cart, Long> {
 
