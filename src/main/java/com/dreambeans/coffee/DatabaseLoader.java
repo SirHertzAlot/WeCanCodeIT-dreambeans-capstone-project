15cd5f489c01268a68ea28435e531aa02d0aa395
SirHertzAlot <Dukjoseph@gmail.com>
2023-09-26 20:45:54 -0400

    moved index.html up one level to make sure spring boot could find it. Also made a home controller to serve the web pages to users when they reach specified urls on website. Lastly commented out DB loader since some of it was giving errors. Will try and fix that later.

--- src/main/java/com/dreambeans/coffee/DatabaseLoader.java
+++ src/main/java/com/dreambeans/coffee/DatabaseLoader.java
@@ -1,31 +1,24 @@
-package com.dreambeans.coffee;
+/*package com.dreambeans.coffee;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.dreambeans.coffee.models.Product;
+import com.dreambeans.coffee.repositories.ProductRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import com.dreambeans.coffee.models.Orders;
-import com.dreambeans.coffee.models.Product;
-import com.dreambeans.coffee.repositories.OrdersRepository;
-import com.dreambeans.coffee.repositories.ProductRepository;
-import com.dreambeans.coffee.service.ProductService;
 
 
 @Component
 public class DatabaseLoader implements CommandLineRunner {
-    private ProductService productService;
     private ProductRepository productRepo;
-    public DatabaseLoader(ProductService productService, ProductRepository productRepo) {
-        this.productService = productService;
+    public DatabaseLoader(ProductRepository productRepo) {
+        this.productRepo = productRepo;
     }
 
     @Override
     public void run(String... args)throws Exception {
         Product latte = new Product(3.99, "latte", "hot coffee", "https://www.allrecipes.com/thmb/Dq9kocJWJNmhfPotGiZT6Tl_r1w=/750x0/filters:no_upscale([…]):format(webp)/9428203-9d140a4ed1424824a7ddd358e6161473.jpg");
-        // productService.saveProduct(latte);
         productRepo.save(latte);
     }
-    }
+}*/
 
 
