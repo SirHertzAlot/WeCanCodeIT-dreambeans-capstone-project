7e8ce2c6db24da93523e15c52f3166597456dd48
krischielle <krischielle.espiritu@gmail.com>
2023-10-10 20:54:34 -0400

    inventory api

--- src/main/resources/static/js/inventory.js
+++ src/main/resources/static/js/inventory.js
@@ -1,10 +1,20 @@
-  const inventoryID = document.getElementsByClassName("inventory-id")[0];
-  const inventoryName = document.getElementsByClassName("inventory-name")[0];
-  const inventoryPrice = document.getElementsByClassName("inventory-price")[0];
-  const inventoryQTY = document.getElementsByClassName("inventory-qty")[0];
-  const inventoryAction = document.getElementsByClassName("inventory-action")[0];
-    function displayInventory(){
-  inventoryData.forEach((item) => {
+
+// this is calling the api for the inventory
+let products;
+fetch("http://localhost:8080/api/products/allproducts")
+  .then((response) => response.json())
+  .then((products) => displayInventory(products))
+  .catch((error) => console.log(error));
+
+const inventoryID = document.getElementsByClassName("inventory-id")[0];
+const inventoryName = document.getElementsByClassName("inventory-name")[0];
+const inventoryPrice = document.getElementsByClassName("inventory-price")[0];
+const inventoryQTY = document.getElementsByClassName("inventory-qty")[0];
+const inventoryAction = document.getElementsByClassName("inventory-action")[0];
+
+function displayInventory(products) {
+  console.log(products);
+  products.forEach((item) => {
     const listItemId = document.createElement("li");
     listItemId.classList.add("inventory-item-id");
     const listItemName = document.createElement("li");
@@ -18,7 +28,7 @@
     listItemId.innerHTML = `<span>${item.id}</span>`;
     listItemName.innerHTML = `<span>${item.name}</span>`;
     listItemPrice.innerHTML = `<span>${item.price}</span>`;
-    listItemQty.innerHTML = `<span>${item.qty}</span>`;
+    listItemQty.innerHTML = `<span>${item.quantity}</span>`;
     listItemAction.innerHTML = `
     <button onclick="editItem(${item.id})"><i class="fa-solid fa-pen-to-square"></i></button>
     <button onclick="deleteItem(${item.id})"><i class="fa-solid fa-trash"></i></button>`;
@@ -28,5 +38,39 @@
     inventoryQTY.appendChild(listItemQty);
     inventoryAction.appendChild(listItemAction);
   });
-    }
-displayInventory();
+
+  const newItem = document.createElement("Item");
+  newItem.innerHTML = `
+        <input class="title-input" placeholder="Title" type="text">
+        <input class="summary-input" placeholder="Summary" type="textbox">
+        <button class="submit-new-item">Submit New Item</button>
+    `;
+  newItem
+    .querySelector(".submit-new-item")
+    .addEventListener("click", (clickEvent) => {
+      clickEvent.preventDefault();
+      console.log("clicked to add item");
+      // clearChildren(mainContent);
+      const itemJson = {
+        id: '0',
+        price: newItem.querySelector("itemPrice").value,
+        description: newItem.querySelector("itemDesc").value,
+        name: newItem.querySelector("itemName").value,
+        image: newItem.querySelector(".itemImage").value,
+        quantity: newItem.querySelector("itemQuantity").value,
+        menuId: newItem.querySelector("itemMenuId").value,
+
+      };
+      console.log(itemJson);
+      fetch("http://localhost:8080/api/products/save", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(itemJson),
+      })
+        .then((response) => response.json())
+        .then((products) => displayInventory(products))
+        .catch((error) => console.log(error));
+    });
+}
